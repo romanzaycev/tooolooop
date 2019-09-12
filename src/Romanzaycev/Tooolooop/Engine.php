dbbed70f5ba01d5f772d84d79b959bfdf731d95a
Roman Zaycev <box@romanzaycev.ru>
2019-09-12 23:32:50 +0700

    V 0.4.1

--- src/Romanzaycev/Tooolooop/Engine.php
+++ src/Romanzaycev/Tooolooop/Engine.php
@@ -28,7 +28,7 @@ use Romanzaycev\Tooolooop\Exceptions\FilterNotFoundException;
 class Engine implements EngineInterface
 {
 
-    const VERSION = '0.4.1';
+    const VERSION = '0.4.2';
 
     /**
      * @var string
